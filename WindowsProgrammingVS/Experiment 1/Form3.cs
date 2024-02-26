ad88e8ae8c4eeda7a29f0898629ada602c89d56d
pranav072003 <142641481+pranav072003@users.noreply.github.com>
2024-02-26 13:47:32 +0530

    Add files via upload

--- WindowsProgrammingVS/Experiment 1/Form3.cs
+++ WindowsProgrammingVS/Experiment 1/Form3.cs
@@ -37,18 +37,49 @@ namespace Programming_in_Windows_Practical_1
         {
             int a = Int32.Parse(textbox_num1.Text);
             int b = Int32.Parse(textbox_num2.Text);
-            float result = (float)a / (float)b;
-            MessageBox.Show(result.ToString(), "Float Division Result");
+            if(b!=0)
+            {
+                float result = (float)a / (float)b;
+                MessageBox.Show(result.ToString(), "Float Division Result");
+            }
+            else
+            {
+                MessageBox.Show("Denominator cannot be zero!", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+            }
         }
 
         private void button_sqrt_Click(object sender, EventArgs e)
         {
             int a = Int32.Parse(textbox_num1.Text);
             int b = Int32.Parse(textbox_num2.Text);
-            double result1 = Math.Sqrt((double)a);
-            double result2 = Math.Sqrt((double)b);
-            MessageBox.Show(result1.ToString(), "Square Root Result of 1st Number");
-            MessageBox.Show(result2.ToString(), "Square Root Result of 2nd Number");
+            if(a>=0 && b>=0)
+            {
+                double result1 = Math.Sqrt((double)a);
+                double result2 = Math.Sqrt((double)b);
+                MessageBox.Show(result1.ToString(), "Square Root Result of 1st Number");
+                MessageBox.Show(result2.ToString(), "Square Root Result of 2nd Number");
+            }
+            else if(a<0 && b>=0)
+            {
+                double result1 = Math.Sqrt((double)(-1)*a);
+                double result2 = Math.Sqrt((double)b);
+                MessageBox.Show(result1.ToString()+"j", "Square Root Result of 1st Number");
+                MessageBox.Show(result2.ToString(), "Square Root Result of 2nd Number");
+            }
+            else if(b<0 && a>=0)
+            {
+                double result1 = Math.Sqrt((double)a);
+                double result2 = Math.Sqrt((double)(-1) * b);
+                MessageBox.Show(result1.ToString(), "Square Root Result of 1st Number");
+                MessageBox.Show(result2.ToString() + "j", "Square Root Result of 2nd Number");
+            }
+            else
+            {
+                double result1 = Math.Sqrt((double)(-1) *a);
+                double result2 = Math.Sqrt((double)(-1) * b);
+                MessageBox.Show(result1.ToString() + "j", "Square Root Result of 1st Number");
+                MessageBox.Show(result2.ToString() + "j", "Square Root Result of 2nd Number");
+            }
         }
 
         private void button_back_Click(object sender, EventArgs e)
