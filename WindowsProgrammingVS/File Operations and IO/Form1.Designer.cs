4df664908367895da5c4ff9c89956f727f9f4bb5
pranav072003 <142641481+pranav072003@users.noreply.github.com>
2024-04-17 12:48:20 +0530

    Add files via upload

--- WindowsProgrammingVS/File Operations and IO/Form1.Designer.cs
+++ WindowsProgrammingVS/File Operations and IO/Form1.Designer.cs
@@ -32,10 +32,19 @@
             button2 = new Button();
             richTextBox1 = new RichTextBox();
             button_continue = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
+            button1.Anchor = AnchorStyles.Bottom;
             button1.Location = new Point(327, 267);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
@@ -46,6 +55,7 @@
             // 
             // button2
             // 
+            button2.Anchor = AnchorStyles.Bottom;
             button2.Location = new Point(441, 265);
             button2.Name = "button2";
             button2.Size = new Size(94, 32);
@@ -57,14 +67,15 @@
             // richTextBox1
             // 
             richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            richTextBox1.Location = new Point(216, 47);
+            richTextBox1.Location = new Point(393, 47);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(415, 195);
+            richTextBox1.Size = new Size(281, 195);
             richTextBox1.TabIndex = 2;
             richTextBox1.Text = "";
             // 
             // button_continue
             // 
+            button_continue.Anchor = AnchorStyles.Bottom;
             button_continue.Location = new Point(381, 316);
             button_continue.Name = "button_continue";
             button_continue.Size = new Size(94, 29);
@@ -73,11 +84,104 @@
             button_continue.UseVisualStyleBackColor = true;
             button_continue.Click += button_continue_Click;
             // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Bottom;
+            button3.Location = new Point(259, 316);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 4;
+            button3.Text = "Write Text";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Bottom;
+            button4.Location = new Point(502, 316);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 5;
+            button4.Text = "Read Text";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(165, 100);
+            label1.Name = "label1";
+            label1.Size = new Size(222, 24);
+            label1.TabIndex = 6;
+            label1.Text = "Enter file text here:- ";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Font = new Font("Tahoma", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(22, 377);
+            label2.Name = "label2";
+            label2.Size = new Size(365, 24);
+            label2.TabIndex = 7;
+            label2.Text = "Enter file position where to write:- ";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(393, 378);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(281, 27);
+            textBox1.TabIndex = 8;
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Bottom;
+            button5.Location = new Point(566, 265);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 32);
+            button5.TabIndex = 9;
+            button5.Text = "Delete File";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Anchor = AnchorStyles.Bottom;
+            button6.Location = new Point(617, 316);
+            button6.Name = "button6";
+            button6.Size = new Size(139, 29);
+            button6.TabIndex = 10;
+            button6.Text = "Get No. of Lines";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Anchor = AnchorStyles.Bottom;
+            button7.Location = new Point(197, 267);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 32);
+            button7.TabIndex = 11;
+            button7.Text = "Delete Line";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
             Controls.Add(button_continue);
             Controls.Add(richTextBox1);
             Controls.Add(button2);
@@ -85,6 +189,7 @@
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -93,5 +198,13 @@
         private Button button2;
         private RichTextBox richTextBox1;
         private Button button_continue;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button5;
+        private Button button6;
+        private Button button7;
     }
 }
--- WindowsProgrammingVS/File Operations and IO/Form1.cs
+++ WindowsProgrammingVS/File Operations and IO/Form1.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace File_operations_in_dotnet
 {
     public partial class Form1 : Form
@@ -10,17 +12,18 @@ namespace File_operations_in_dotnet
         private void button1_Click(object sender, EventArgs e)
         {
             string path = "C:/Users/Pranav Jain/Desktop/Gate paper 2024/6th Semester/Prgm in Windows/demo-file.txt";
-            string folder_path = "C:/Users/Pranav Jain/Desktop/Gate paper 2024/6th Semester/Prgm in Windows/demo-folder";
+            // string folder_path = "C:/Users/Pranav Jain/Desktop/Gate paper 2024/6th Semester/Prgm in Windows/demo-folder";
             if (File.Exists(path))
             {
                 // check whether a file with the same path already exists or not
                 DialogResult result = MessageBox.Show("File already exists!");
+                return;
             }
-            if (!Directory.Exists(folder_path))
+            /*if (!Directory.Exists(folder_path))
             {
                 // check whether a folder with name does not exists or not
                 Directory.CreateDirectory(folder_path);
-            }
+            }*/
 
             // can alternatively use StreamWriter writer = new StreamWriter(path); also
             // create and write on created file (not inside new created directory)
@@ -37,8 +40,8 @@ namespace File_operations_in_dotnet
             // above line works given the streamwriter is closed appropriately after the write operation
             // has been performed
 
-            string file_folder_path = Path.Combine(folder_path, "demo-file.txt");
-            File.Create(file_folder_path).Close(); // create a file and close it immediately
+            // string file_folder_path = Path.Combine(folder_path, "demo-file.txt");
+            // File.Create(file_folder_path).Close(); // create a file and close it immediately
             DialogResult r = MessageBox.Show("Operations applied successfully!");
         }
 
@@ -52,6 +55,134 @@ namespace File_operations_in_dotnet
             Form2 f = new Form2();
             f.Show();
             Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string path = "C:/Users/Pranav Jain/Desktop/Gate paper 2024/6th Semester/Prgm in Windows/demo-file.txt";
+
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("File does not exists");
+                return;
+            }
+
+            /* string file_text;
+            if (textBox1.Text.Length != 0)
+            {
+                // take in account the position at which text to be written
+                int pos_idx = Convert.ToInt32(textBox1.Text);
+                StreamReader sr = new StreamReader(path);
+                file_text = sr.ReadToEnd();
+                sr.Close();  // careful with this: If wither of streamreader or streamwriter left open, unhandled exception error raised
+                if (pos_idx > file_text.Length || pos_idx <= 0)
+                {
+                    MessageBox.Show("Invalid Index! Please try again!");
+                    return;
+                }
+                // append the user entered text to the file at the given position index
+                string substr1 = file_text.Substring(0, pos_idx - 1);
+                string substr2 = file_text.Substring(pos_idx - 1, file_text.Length - pos_idx + 1);
+                file_text = substr1 + " " +  richTextBox1.Text + " " + substr2;
+                MessageBox.Show(file_text);
+                return;
+            }  taken by assuming data to be written at particular index of data */
+
+            // assuming new data to be written at a line index in file
+            if (textBox1.Text.Length != 0)
+            {
+                int lineCount = File.ReadLines(path).Count();
+                if (Convert.ToInt32(textBox1.Text) <= lineCount)
+                {
+                    string[] lines = File.ReadAllLines(path);
+                    if (lines[Convert.ToInt32(textBox1.Text) - 1] == "")
+                    {
+                        // line null
+                        lines[Convert.ToInt32(textBox1.Text) - 1] = richTextBox1.Text;
+                        File.WriteAllLines(path, lines);
+                        MessageBox.Show("Operations applied successfully!");
+                        return;
+                    }
+                    else
+                    {
+                        // line not null, data cannot be written
+                        MessageBox.Show("Data cannot be overwritten, line already present.");
+                        return;
+                    }
+                }
+                StreamWriter sw_append = new StreamWriter(path, true);
+                for (int i = (lineCount + 1); i < Convert.ToInt32(textBox1.Text); i++)
+                {
+                    // keep writing empty lines over the file
+                    sw_append.WriteLine("");
+                }
+                sw_append.WriteLine(richTextBox1.Text);
+                sw_append.Close();
+                MessageBox.Show("Operations applied successfully!");
+                return;
+            }
+
+            // else append lines to the data of file
+            // can use this if to append data to next line
+            StreamWriter sw = new StreamWriter(path, true);
+            sw.WriteLine(richTextBox1.Text);
+            sw.Close();
+            DialogResult r = MessageBox.Show("Operations applied successfully!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string path = "C:/Users/Pranav Jain/Desktop/Gate paper 2024/6th Semester/Prgm in Windows/demo-file.txt";
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("File does not exists");
+                return;
+            }
+            StreamReader sr = new StreamReader(path);
+            richTextBox1.Text = sr.ReadToEnd();
+            sr.Close();
+            DialogResult r = MessageBox.Show("Operations applied successfully!");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string path = "C:/Users/Pranav Jain/Desktop/Gate paper 2024/6th Semester/Prgm in Windows/demo-file.txt";
+            File.Delete(path);
+            MessageBox.Show("File successfully deleted");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string path = "C:/Users/Pranav Jain/Desktop/Gate paper 2024/6th Semester/Prgm in Windows/demo-file.txt";
+            if(!File.Exists(path))
+            {
+                return;
+            }
+            int lineCount = File.ReadLines(path).Count();
+            MessageBox.Show(lineCount.ToString());
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string path = "C:/Users/Pranav Jain/Desktop/Gate paper 2024/6th Semester/Prgm in Windows/demo-file.txt";
+
+            if (textBox1.Text.Length == 0)
+            {
+                // no input entered
+                MessageBox.Show("No input. Please try again.");
+                return;
+            }
+
+            if (!File.Exists(path))
+            {
+                return;
+            }
+
+            string[] lines = File.ReadAllLines(path);   // reading all lines of file
+            int idx = Convert.ToInt32(textBox1.Text);
+            lines[idx - 1] = ""; // deleting the line
+            File.WriteAllLines(path, lines);
+            MessageBox.Show("Operation applied successfully!");
         }
     }
 }
