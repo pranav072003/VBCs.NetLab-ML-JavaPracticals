23262994eb858f8887f50efa7633ce2d95573317
pranav072003 <142641481+pranav072003@users.noreply.github.com>
2024-04-19 21:40:52 +0530

    Add files via upload

---
+++ WindowsProgrammingVS/Database Connectivity in Windows Forms/C#.Net Database Connectivity.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE Pranav;
+
+use Pranav;   -- need to use the database, the table needs to be stored in it
+
+/* CREATE TABLE MyFirstTable(
+ID INT NOT NULL IDENTITY(1,1),
+NAME VARCHAR(60),
+COURSE VARCHAR(60)
+); */
+
+-- INSERT INTO MyFirstTable(NAME,COURSE) VALUES('Pranav', 'B.Tech CSE');
+
+Select * from MyFirstTable;
