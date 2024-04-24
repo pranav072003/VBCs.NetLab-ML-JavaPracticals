3a849669b42c2bb8f0eacc94b1fe4c93f777ee80
pranav072003 <142641481+pranav072003@users.noreply.github.com>
2024-04-24 09:32:58 +0530

    Add files via upload

--- WindowsProgrammingVS/Database Connectivity in Windows Forms/Form1.Designer.cs
+++ WindowsProgrammingVS/Database Connectivity in Windows Forms/Form1.Designer.cs
@@ -35,28 +35,31 @@
             label2 = new Label();
             button_exit = new Button();
             button_restart = new Button();
+            label4 = new Label();
+            listbox_studentdetail = new ListBox();
+            button_reset = new Button();
             SuspendLayout();
             // 
             // textbox_name
             // 
-            textbox_name.Anchor = AnchorStyles.Bottom;
-            textbox_name.Location = new Point(345, 104);
+            textbox_name.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            textbox_name.Location = new Point(255, 104);
             textbox_name.Name = "textbox_name";
             textbox_name.Size = new Size(270, 27);
             textbox_name.TabIndex = 1;
             // 
             // textbox_course
             // 
-            textbox_course.Anchor = AnchorStyles.Bottom;
-            textbox_course.Location = new Point(345, 161);
+            textbox_course.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            textbox_course.Location = new Point(255, 162);
             textbox_course.Name = "textbox_course";
             textbox_course.Size = new Size(270, 27);
             textbox_course.TabIndex = 3;
             // 
             // button_database
             // 
-            button_database.Anchor = AnchorStyles.Bottom;
-            button_database.Location = new Point(345, 240);
+            button_database.Anchor = AnchorStyles.Top;
+            button_database.Location = new Point(566, 104);
             button_database.Name = "button_database";
             button_database.Size = new Size(200, 29);
             button_database.TabIndex = 4;
@@ -66,9 +69,9 @@
             // 
             // label1
             // 
-            label1.Anchor = AnchorStyles.Bottom;
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             label1.AutoSize = true;
-            label1.Location = new Point(229, 104);
+            label1.Location = new Point(133, 104);
             label1.Name = "label1";
             label1.Size = new Size(49, 20);
             label1.TabIndex = 5;
@@ -76,18 +79,18 @@
             // 
             // label2
             // 
-            label2.Anchor = AnchorStyles.Bottom;
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             label2.AutoSize = true;
-            label2.Location = new Point(229, 161);
+            label2.Location = new Point(95, 160);
             label2.Name = "label2";
-            label2.Size = new Size(54, 20);
+            label2.Size = new Size(139, 20);
             label2.TabIndex = 6;
-            label2.Text = "Course";
+            label2.Text = "Enrollment Number";
             // 
             // button_exit
             // 
-            button_exit.Anchor = AnchorStyles.Bottom;
-            button_exit.Location = new Point(345, 292);
+            button_exit.Anchor = AnchorStyles.Top;
+            button_exit.Location = new Point(566, 160);
             button_exit.Name = "button_exit";
             button_exit.Size = new Size(200, 29);
             button_exit.TabIndex = 7;
@@ -97,8 +100,8 @@
             // 
             // button_restart
             // 
-            button_restart.Anchor = AnchorStyles.Bottom;
-            button_restart.Location = new Point(345, 346);
+            button_restart.Anchor = AnchorStyles.Top;
+            button_restart.Location = new Point(566, 218);
             button_restart.Name = "button_restart";
             button_restart.Size = new Size(200, 29);
             button_restart.TabIndex = 8;
@@ -106,11 +109,48 @@
             button_restart.UseVisualStyleBackColor = true;
             button_restart.Click += button_restart_Click;
             // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            label4.AutoSize = true;
+            label4.Font = new Font("Showcard Gothic", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(198, 37);
+            label4.Name = "label4";
+            label4.Size = new Size(468, 29);
+            label4.TabIndex = 10;
+            label4.Text = "Student Database Registration Form";
+            // 
+            // listbox_studentdetail
+            // 
+            listbox_studentdetail.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            listbox_studentdetail.FormattingEnabled = true;
+            listbox_studentdetail.Items.AddRange(new object[] { "Following are the USSs. Choose one of them:-", "USICT", "USLLS", "USBAS", "USAP" });
+            listbox_studentdetail.Location = new Point(95, 218);
+            listbox_studentdetail.Name = "listbox_studentdetail";
+            listbox_studentdetail.Size = new Size(444, 144);
+            listbox_studentdetail.TabIndex = 13;
+            listbox_studentdetail.SelectedIndexChanged += listbox_studentdetail_SelectedIndexChanged;
+            // 
+            // button_reset
+            // 
+            button_reset.Anchor = AnchorStyles.Top;
+            button_reset.Location = new Point(566, 270);
+            button_reset.Name = "button_reset";
+            button_reset.Size = new Size(200, 29);
+            button_reset.TabIndex = 14;
+            button_reset.Text = "Reset ";
+            button_reset.UseVisualStyleBackColor = true;
+            button_reset.Click += button_reset_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
             ClientSize = new Size(800, 450);
+            Controls.Add(button_reset);
+            Controls.Add(listbox_studentdetail);
+            Controls.Add(label4);
             Controls.Add(button_restart);
             Controls.Add(button_exit);
             Controls.Add(label2);
@@ -120,6 +160,7 @@
             Controls.Add(textbox_name);
             Name = "Form1";
             Text = "Database Connectivity";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -132,5 +173,8 @@
         private Label label2;
         private Button button_exit;
         private Button button_restart;
+        private Label label4;
+        private ListBox listbox_studentdetail;
+        private Button button_reset;
     }
 }
--- WindowsProgrammingVS/Database Connectivity in Windows Forms/Form1.cs
+++ WindowsProgrammingVS/Database Connectivity in Windows Forms/Form1.cs
@@ -4,6 +4,11 @@ namespace Database_Connectivity_in_Windows_Forms
 {
     public partial class Form1 : Form
     {
+        // creating three strings for dept, course
+        public string dept = "";
+        public string course = "";
+        public string[] original_collection_list;  // original list items stored in listbox
+
         public Form1()
         {
             InitializeComponent();
@@ -11,16 +16,53 @@ namespace Database_Connectivity_in_Windows_Forms
 
         private void button_database_Click(object sender, EventArgs e)
         {
-            string Connectionstring = "Data Source=LAPTOP-NFJ7VAOT\\SQLEXPRESS;Initial Catalog=Pranav;Integrated Security=True;Connect Timeout=30;Encrypt=False"; // Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
-            SqlConnection con = new SqlConnection(Connectionstring);
-            con.Open();
-            string name = textbox_name.Text;
-            string course = textbox_course.Text;
-            string Query = "INSERT INTO MyFirstTable(NAME, COURSE) VALUES('" + name + "', '" + course + "')";
-            SqlCommand cmd = new SqlCommand(Query, con);
-            cmd.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Data Saved Successfully");
+            // Create a form with multiple fields as given in the SSMS file for SQL server
+            try
+            {
+                DialogResult d = MessageBox.Show("Are you sure you want to submit? No updates are allowed once the data has been submitted!","Alert",MessageBoxButtons.YesNo,MessageBoxIcon.Information);
+                if (d == DialogResult.No)
+                {
+                    // return and do not proceed with saving
+                    return;
+                }
+                string Connectionstring = "Data Source=LAPTOP-NFJ7VAOT\\SQLEXPRESS;Initial Catalog=Pranav;Integrated Security=True;Connect Timeout=30;Encrypt=False"; // Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+                SqlConnection con = new SqlConnection(Connectionstring);
+                con.Open();
+                string name = textbox_name.Text;
+                string enroll_id = textbox_course.Text;
+                if (dept.Length == 0 || course.Length == 0)
+                {
+                    // close the database connection
+                    MessageBox.Show("Entries cannot be made to database");
+                    con.Close();
+                    return;
+                }
+
+                // for ensuring no multiple entries for same enrollment numbers are made
+                string find_query = "SELECT COUNT(*) from MyFirstTable where ENROLLMENT_NUM='" + enroll_id + "'";
+                SqlCommand cmd_find = new SqlCommand(find_query, con);
+                Int32 r = Convert.ToInt32(cmd_find.ExecuteScalar());
+                if (r!=0)
+                {
+                    // entry already exists for the enrollment id
+                    MessageBox.Show("Entry already exists for the current student!");
+                    con.Close();
+                    return;
+                }
+
+                MessageBox.Show(name + " " + enroll_id + " " + dept + " " + course);
+                string Query = "INSERT INTO MyFirstTable(NAME,ENROLLMENT_NUM,DEPARTMENT,COURSE) VALUES('" + name + "', '" + enroll_id + "', '" + dept + "', '" + course + "')";
+                SqlCommand cmd = new SqlCommand(Query, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Data Saved Successfully");
+            }
+            catch (SqlException ex)  // SqlException class for displaying the errors while resolving SQL queries
+            {
+                // print the exception message
+                MessageBox.Show("Query resolution error! Entries not satisfying the database constraints!");
+                return;
+            }
         }
 
         private void button_exit_Click(object sender, EventArgs e)
@@ -31,6 +73,117 @@ namespace Database_Connectivity_in_Windows_Forms
         private void button_restart_Click(object sender, EventArgs e)
         {
             Application.Restart();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ToolTip t = new ToolTip();
+            t.SetToolTip(button_database, "Click here to save data to database");
+            t.SetToolTip(button_exit, "Click here to exit the application");
+            t.SetToolTip(button_restart, "Click here to restart the application");
+            t.SetToolTip(listbox_studentdetail, "Click here to select more details");
+            t.SetToolTip(button_reset, "Click here to reset the settings");
+            original_collection_list = new string[listbox_studentdetail.Items.Count];
+            for (int i = 0; i < listbox_studentdetail.Items.Count; i++)
+            {
+                original_collection_list[i] = listbox_studentdetail.Items[i].ToString();
+            }
+        }
+
+        private void listbox_studentdetail_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // check for definition of dept and course strings
+            if (dept.Length != 0 && course.Length != 0)
+            {
+                MessageBox.Show("Please reset the listbox before proceeding...");
+                return;
+            }
+            // create string for menu title of courses
+            string courses = "Choose one of the courses";
+            if (listbox_studentdetail.SelectedIndex == 0 || listbox_studentdetail.SelectedItems.Count > 1)
+            {
+                // return error
+                MessageBox.Show("Navigation Error....");
+                return;
+            }
+            if (listbox_studentdetail.Items[0] == "Following are the USSs. Choose one of them:-")
+            {
+                // put selected item into dept string
+                dept = listbox_studentdetail.SelectedItem.ToString();
+
+                // create list of subjects for each of the options
+                string[] USICT = new string[] { "B.Tech", "M.Tech", "MCA", "Ph.D" };
+                string[] USLLS = new string[] { "BA LLB", "BBA LLB", "LLM" };
+                string[] USBAS = new string[] { "M.Tech (Engineering Physics)", "M.Tech (Nanoscience and Technology)", "Ph.D" };
+                string[] USAP = new string[] { "B.Arch", "M.Arch", "M.Plan", "Ph.D" };
+
+                // based on the options selected create new menu
+                if (listbox_studentdetail.SelectedIndex == 1)
+                {
+                    // USICT
+                    listbox_studentdetail.Items.Clear();
+                    listbox_studentdetail.Items.Add(courses);
+                    for (int i = 0; i < USICT.Length; i++)
+                    {
+                        listbox_studentdetail.Items.Add(USICT[i]);
+                    }
+                }
+                else if (listbox_studentdetail.SelectedIndex == 2)
+                {
+                    // USLLS
+                    listbox_studentdetail.Items.Clear();
+                    listbox_studentdetail.Items.Add(courses);
+                    for (int i = 0; i < USLLS.Length; i++)
+                    {
+                        listbox_studentdetail.Items.Add(USLLS[i]);
+                    }
+                }
+                else if (listbox_studentdetail.SelectedIndex == 3)
+                {
+                    // USBAS
+                    listbox_studentdetail.Items.Clear();
+                    listbox_studentdetail.Items.Add(courses);
+                    for (int i = 0; i < USBAS.Length; i++)
+                    {
+                        listbox_studentdetail.Items.Add(USBAS[i]);
+                    }
+                }
+                else
+                {
+                    // USAP
+                    listbox_studentdetail.Items.Clear();
+                    listbox_studentdetail.Items.Add(courses);
+                    for (int i = 0; i < USAP.Length; i++)
+                    {
+                        listbox_studentdetail.Items.Add(USAP[i]);
+                    }
+                }
+            }
+            else if (listbox_studentdetail.Items[0] == courses)
+            {
+                // put selected item into course string
+                course = listbox_studentdetail.SelectedItem.ToString();
+                // reset listbox to original list of items
+                listbox_studentdetail.Items.Clear();
+                for (int i = 0; i < original_collection_list.Length; i++)
+                {
+                    listbox_studentdetail.Items.Add(original_collection_list[i]);
+                }
+                MessageBox.Show("Student Department: " + dept + ",Course Enrolled into: " + course);
+            }
+        }
+
+        private void button_reset_Click(object sender, EventArgs e)
+        {
+            listbox_studentdetail.Items.Clear();
+            textbox_course.Clear();
+            textbox_course.Focus();
+            textbox_name.Clear();
+            textbox_name.Focus();
+            for(int i=0;i<original_collection_list.Length;i++)
+            {
+                listbox_studentdetail.Items.Add(original_collection_list[i]);
+            }
         }
     }
 }
