39b50dc1fef2aaa08807b0acfd0bb3107936627f
pranav072003 <142641481+pranav072003@users.noreply.github.com>
2024-05-09 19:36:53 +0530

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim t As New ToolTip()
+        t.SetToolTip(button_showprime, "Click here to add prime numbers between 1 and 100")
+        t.SetToolTip(button_exit, "Click here to exit the application")
+        t.SetToolTip(button_continue, "Click here to move ahead....")
+        t.SetToolTip(button_reset, "Click here to reset the listbox")
+    End Sub
+
+    Private Sub button_exit_Click(sender As Object, e As EventArgs) Handles button_exit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub button_showprime_Click(sender As Object, e As EventArgs) Handles button_showprime.Click
+        For i = 1 To 100 Step 1
+            If i = 2 Or i = 3 Then
+                Listbox_primenum.Items.Add(i)
+                Continue For
+            ElseIf i = 1 Then
+                Continue For
+            End If
+            Dim j As Integer = 2
+            While j <= Math.Floor(Math.Sqrt(i))
+                If i Mod j = 0 Then
+                    Continue For
+                End If
+                j += 1
+            End While
+            Listbox_primenum.Items.Add(i)
+        Next
+    End Sub
+
+    Private Sub button_continue_Click(sender As Object, e As EventArgs) Handles button_continue.Click
+        Dim f As New Form2()
+        Hide()
+        f.Show()
+    End Sub
+
+    Private Sub button_reset_Click(sender As Object, e As EventArgs) Handles button_reset.Click
+        Listbox_primenum.Items.Clear()
+        Listbox_primenum.Focus()
+        Listbox_primenum.Items.Add("The prime numbers between 1 and 100 are shown as:-")
+    End Sub
+End Class
---
+++ Form2.vb
@@ -0,0 +1,58 @@
+﻿Imports System.Runtime.Intrinsics.X86
+
+Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim t As New ToolTip()
+        t.SetToolTip(button_sequence, "Click here to print sequence")
+        t.SetToolTip(button_exit, "Click here to exit the application")
+        t.SetToolTip(button_back, "Click here to go back....")
+        t.SetToolTip(button_reset, "Click here to reset the listbox")
+    End Sub
+
+    Private Sub button_back_Click(sender As Object, e As EventArgs) Handles button_back.Click
+        Dim f As New Form1()
+        Hide()
+        f.Show()
+    End Sub
+
+    Private Sub button_exit_Click(sender As Object, e As EventArgs) Handles button_exit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub button_sequence_Click(sender As Object, e As EventArgs) Handles button_sequence.Click
+        Dim a As Integer
+        Try
+            a = Convert.ToInt32(textbox_num.Text)
+        Catch ex As Exception
+            textbox_num.Text = "Error! Please try again"
+        End Try
+        ' proceed with building the series
+        Dim levels As Integer = a
+        ' check or levels condition
+        If levels = 0 Then
+            MessageBox.Show("Invalid input! Try again!")
+            Return
+        End If
+        Dim k As Integer = 1
+        Dim space As Integer = levels
+        For i = 1 To levels Step 1
+            Dim s As String = Strings.StrDup(levels + 2, " ")
+            ' TODO: add logic for adding numbers in a row to form star like pattern
+            Dim idx As Integer = space - 1
+            For j = k To (k + i - 1)
+                s = s.Insert(idx, j.ToString())
+                idx += 2
+            Next
+            k += i
+            space -= 1
+            listbox_sequence.Items.Add(s)
+        Next
+    End Sub
+
+    Private Sub button_reset_Click(sender As Object, e As EventArgs) Handles button_reset.Click
+        listbox_sequence.Items.Clear()
+        listbox_sequence.Focus()
+        textbox_num.Clear()
+        textbox_num.Focus()
+    End Sub
+End Class
